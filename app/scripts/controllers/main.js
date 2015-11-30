f7097bbe1d530c02b2f600b77aa3126d2c02a308
Marcin Ziółek <ziolkenz@interia.pl>
2015-11-30 17:44:15 +0100

    Init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name registrationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the registrationApp
+ */
+angular.module('registrationApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
